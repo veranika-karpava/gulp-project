c486534013dbfe7a89fc50de6b7f6464d7b458fa
veranika-karpava <veranika.karpava@gmail.com>
2022-03-15 00:59:16 -0700

    Fix bug in project Info section

--- src/components/TaskInfo.js
+++ src/components/TaskInfo.js
@@ -1,64 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import taskData from '../data/task.json';
 import sort from '../assets/icons/sort.svg';
 
-const defaultData =
-    [
-        'Task name',
-        'Developer',
-        'Work Type',
-        'Status',
-        'Estimation (h)',
-        'Total time spent by All',
-        'My Time spent by Period(h)',
-        'Efficiency',
-    ]
-
 const TaskInfo = ({ data, isSelectedDev }) => {
-    // const [taskListData, setIsTaskListData] = useState([])
-    // console.log(taskListData)
+    const [isShowed, setIsShowed] = useState(false)
 
-    const handleSorting = (e, key) => {
-
-        const taskSortedListData = taskListData.sort((a, b) => (a.key < b.key ? -1 : 1));
-
-        console.log(taskSortedListData)
-        console.log("it works")
-    }
-
-    // filter data depends on selected Dev and name of project
-    const filterDataTask = () => {
-        const devIndex = taskData.findIndex((dev) => dev.devName === isSelectedDev);
-        const projectIndex = taskData[devIndex].projectList.findIndex((item) => item.projectName === data.projectName);
-
-        const taskListData = ((devIndex < 0 || projectIndex < 0) ?
-            [] : taskData[devIndex].projectList[projectIndex].taskList);
-
-        return taskListData;
-    }
-
-    const taskListData = filterDataTask();
-
-    // for rendering data from taskListData
-    const renderTableData = () => {
-        return taskListData.map((item, index) => {
-            const { taskName, developer, workType, status, estimation, totalTimeSpentByAll, myTimeSpentByPeriod, efficiency } = item
-            return (
-                <tr className='results__row-container' key={index}>
-                    <td className='results__data results__data-name'>{taskName}</td>
-                    <td className='results__data results__developer'>{developer}</td>
-                    <td className='results__data results__work'>{workType}</td>
-                    {status === "Completed" ? <td className='results__data results__status-completed'>{status}</td> : <td className='results__data results__status-non'>{status}</td>}
-                    <td className='results__data'>{estimation}</td>
-                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{totalTimeSpentByAll}h</td> : <td className='results__data'>{totalTimeSpentByAll}</td>}
-
-                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{myTimeSpentByPeriod}h</td> : <td className='results__data'>{myTimeSpentByPeriod}</td>}
-
-                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{efficiency}({myTimeSpentByPeriod}h)</td> : <td className='results__data'>{efficiency}</td>}
-                </tr>
-            )
-        })
-    }
+    const defaultData =
+        [
+            'Task name',
+            'Developer',
+            'Work Type',
+            'Status',
+            'Estimation (h)',
+            'Total time spent by All',
+            'My Time spent by Period(h)',
+            'Efficiency',
+        ]
 
     //for rendering heading of table
     const renderTableHeader = () => {
@@ -70,6 +27,114 @@ const TaskInfo = ({ data, isSelectedDev }) => {
                 <img src={sort} alt='Sort icon' className='results__sort-icon' onClick={() => handleSorting('status')} />
             </th>
         })
+    }
+
+    // filter data depends on selected Dev and name of project
+    const filterDataTask = () => {
+        // index of selected Dev
+        const devIndex = taskData.findIndex((dev) => dev.devName === isSelectedDev);
+        const projectIndex = taskData[devIndex].projectList.findIndex((item) => item.projectName === data.projectName);
+        const taskListData = ((devIndex < 0 || projectIndex < 0) ?
+            [] : taskData[devIndex].projectList[projectIndex].taskList);
+        return taskListData;
+    }
+
+    const taskListData = filterDataTask();
+
+    // show more option in table 
+    const showToggle = (e) => {
+        setIsShowed(!isShowed)
+    }
+
+    const getRenderedItems = (array) => {
+        let showArray = [];
+        let showMoreArray = [];
+        if (!isShowed) {
+            showArray = [...array.splice(0, 2)]
+            console.log(isShowed)
+            console.log(showArray)
+            return showArray;
+        }
+        showMoreArray = [...array]
+        console.log(isShowed)
+
+        console.log(showMoreArray)
+        return showMoreArray;
+    }
+
+
+
+
+    // for rendering data from taskListData
+    const renderTableData = () => {
+        return taskListData.map((item, index) => {
+            const { taskName, developer, workType, status, estimation, totalTimeSpentByAll, myTimeSpentByPeriod, efficiency } = item
+            // console.log(developer)
+
+            return (
+                <tr className='results__row-table-task' key={index}>
+
+                    {taskName === "Sum" ? <td className='results__data results__data-name-sum'>{taskName}</td> : <td className='results__data results__data-name'>{taskName}</td>}
+
+                    {developer.length > 2 ?
+                        <td className='results__data results__developer'>
+                            <p className='results__content-developer'>
+                                {getRenderedItems(developer).map((name, i) => {
+                                    console.log(name)
+                                    return <span key={i}>{`${name}, `}</span>
+                                })}
+                            </p>
+                            {/* <button className='results__button-show-more' onClick={showToggle}>{isShowed ? 'Show less' : `Show more(${developer.length})`}</button> */}
+                        </td>
+                        :
+                        <td className='results__data results__developer'><p className='results__content-developer'>{`${developer}`}</p></td>
+                    }
+
+
+                    {/* <td className='results__data results__developer'>
+                        {getRenderedItems(developer).map((name, i) => {
+                            <>
+                                <p className='results__content-developer' key={i}>{`${name}, `}</p>
+                                <button className='results__button-show-more' onClick={showToggle}>{isShowed ? 'Show less' : `Show more(${developer.length})`}</button>
+                            </>
+                        })}
+                    </td> */}
+
+                    {/* {developer.length > 2 ?
+                        <td className='results__data results__developer'>
+                            {getRenderedItems(developer).map((name, i) => {
+                                console.log(name)
+                                return <p className='results__content-developer' key={i}>{`${name}, `}</p>
+                            })}
+                            {!isShowed ? <button className='results__button-show-more' onClick={showToggle}>{`Show more(${developer.length})`}</button> : <button className='results__button-show-more' onClick={showToggle}>{`Show less(${developer.length})`}</button>}
+                        </td>
+                        : <td className='results__data results__developer'>
+                            {developer}</td>} */}
+
+                    <td className='results__data results__work'>{workType}</td>
+
+                    {status === "Completed" ? <td className='results__data results__status-completed'>{status}</td> : <td className='results__data results__status-non'>{status}</td>}
+
+                    <td className='results__data'>{estimation}</td>
+
+                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{totalTimeSpentByAll}h</td> : <td className='results__data'>{totalTimeSpentByAll}</td>}
+
+                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{myTimeSpentByPeriod}h</td> : <td className='results__data'>{myTimeSpentByPeriod}</td>}
+
+                    {taskName === "Sum" ? <td className='results__data results__data-sum'>{efficiency}({myTimeSpentByPeriod}h)</td> : <td className='results__data'>{efficiency}</td>}
+                </tr >
+            )
+        })
+    }
+
+
+
+    const handleSorting = (e, key) => {
+
+        const taskSortedListData = taskListData.sort((a, b) => (a.key < b.key ? -1 : 1));
+
+        console.log(taskSortedListData)
+        console.log("it works")
     }
 
     return (
